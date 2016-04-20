f0219255c13a3023b6aa85a3e27b12506f732be3
Amin Riza <aminriza@gmail.com>
2016-04-20 12:08:22 +0700

    ui and threads

---
+++ ui.py
@@ -0,0 +1,384 @@
+import logging
+import time
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtWebKit import QWebView
+
+import xthread
+
+
+class MediaStopping:
+    def __init__(self, view):
+        self._view = None
+        if isinstance(view, MediaView):
+            self._view = view
+
+    def wait(self, every=0.1, tries=10):
+        if self._view:
+            while self._view.is_playing() and tries > 0:
+                tries -= 1
+                time.sleep(every)
+
+    def __enter__(self):
+        if self._view:
+            self._view.stop()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+
+class MediaView(QObject):
+    """A view represents Xibo media"""
+    started_signal = Signal()
+    finished_signal = Signal()
+    stopping_signal = Signal(dict)
+    log = logging.getLogger('xiboside.MediaView')
+
+    def __init__(self, parent=None):
+        super(MediaView, self).__init__(parent)
+        self._path = None
+        self._duration = 0
+        self._widget = None
+        self._media = None
+        self._started = False
+        self._playing = False
+        self._finished = False
+        self._ready = False
+        self._play_timer = QTimer(self)
+        self._connect_signals()
+        self.log.setLevel(logging.DEBUG)
+
+    def _connect_signals(self):
+        self.started_signal.connect(self.mark_started)
+        self.finished_signal.connect(self.mark_finished)
+        self._play_timer.setSingleShot(True)
+        self.connect(self._play_timer, SIGNAL("timeout()"), self.stop)
+
+    @Slot()
+    def play(self):
+        pass
+
+    @Slot()
+    def stop(self):
+        pass
+
+    @Slot()
+    def mark_started(self):
+        self._started = True
+
+    @Slot()
+    def mark_finished(self):
+        self._finished = True
+
+    def is_ready(self):
+        return self._ready
+
+    def is_started(self):
+        return self._started
+
+    def is_playing(self):
+        return self.is_started() and not self.is_finished()
+
+    def is_finished(self):
+        return self._finished
+
+    def set_media(self, media):
+        self._media = media
+
+    def set_default_widget_prop(self):
+        if self._widget is not None:
+            self._widget.setAttribute(Qt.WA_DeleteOnClose, False)
+            self._widget.setFocusPolicy(Qt.NoFocus)
+            self._widget.setContextMenuPolicy(Qt.NoContextMenu)
+
+    def widget(self):
+        return self._widget
+
+    def set_geometry(self, left=0, top=0, width=100, height=100):
+        self._widget.setGeometry(int(left), int(top), int(width), int(height))
+
+
+class ImageMediaView(MediaView):
+    def __init__(self, media=None, parent=None):
+        super(ImageMediaView, self).__init__(parent)
+        if media:
+            self.set_media(media)
+        self._widget = QLabel(parent)
+        self.set_default_widget_prop()
+
+    @Slot()
+    def play(self):
+        self.set_geometry(self._media['region']['left'], self._media['region']['top'],
+                          self._media['region']['width'], self._media['region']['height'])
+
+        path = "%s/%s" % (self._media['save_dir'], self._media['options']['uri'])
+
+        rect = self._widget.geometry()
+        img = QImage(path).scaled(rect.width(), rect.height(),
+                                  Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        self._widget.setPixmap(QPixmap.fromImage(img))
+        self._widget.show()
+        duration = int(self._media['duration']) * 1000
+        self._play_timer.start(duration)
+        self.started_signal.emit()
+
+    @Slot()
+    def stop(self):
+        self.stopping_signal.emit(self._media)
+        if self._widget:
+            while not self._widget.close():
+                time.sleep(0.25)
+            del self._widget
+            self._widget = None
+
+        self.finished_signal.emit()
+
+
+class VideoMediaView(MediaView):
+    def __init__(self, media, parent=None):
+        super(VideoMediaView, self).__init__(parent)
+        if media:
+            self.set_media(media)
+        self.process = QProcess(parent)
+        self._widget = QWidget(parent)
+        self.set_default_widget_prop()
+        self.connect(self.process, SIGNAL("readyReadStandardOutput()"), self.__grep_std_out)
+        self.std_out = []
+
+    @Slot()
+    def play(self):
+        self.set_geometry(self._media['region']['left'], self._media['region']['top'],
+                          self._media['region']['width'], self._media['region']['height'])
+
+        path = "%s/%s" % (self._media['save_dir'], self._media['options']['uri'])
+
+        self._widget.show()
+        args = [
+            '-slave', '-identify', '-input',
+            'nodefault-bindings:conf=/dev/null',
+            '-wid', str(int(self._widget.winId())),
+            path
+        ]
+        self.process.start('mplayer', args)
+        self.started_signal.emit()
+
+    @Slot()
+    def stop(self):
+        self.stopping_signal.emit(self._media)
+        if self.process.isOpen():
+            self.process.write("stop\n")
+            if not self.process.waitForFinished(500):
+                self.process.write("quit\n")
+            if not self.process.waitForFinished(500):
+                self.process.kill()
+
+        if self.process.isOpen():
+            self.process.close()
+
+        if self._widget:
+            while not self._widget.close():
+                time.sleep(0.25)
+            del self._widget
+            self._widget = None
+        self.finished_signal.emit()
+
+    @Slot()
+    def __grep_std_out(self):
+        lines = self.process.readAllStandardOutput().split("\n")
+        for line in lines:
+            if not line.isEmpty():
+                if line.startsWith("Starting playback"):
+                    pass
+                else:
+                    part = line.split("=")
+                    if 'ID_EXIT' == part[0]:
+                        self.stop()
+                    elif 'ID_LENGTH' == part[0]:
+                        self._play_timer.stop()
+                        self._play_timer.start(1000 * float(part[1]))
+
+
+class WebMediaView(MediaView):
+    def __init__(self, media, parent=None):
+        super(WebMediaView, self).__init__(parent)
+        if media:
+            self.set_media(media)
+        self._widget = QWebView(parent)
+        self._widget.setDisabled(True)
+        self._widget.page().mainFrame().setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAlwaysOff)
+        self._widget.page().mainFrame().setScrollBarPolicy(Qt.Horizontal, Qt.ScrollBarAlwaysOff)
+        self.set_default_widget_prop()
+
+    @Slot()
+    def play(self):
+        self.set_geometry(self._media['region']['left'], self._media['region']['top'],
+                          self._media['region']['width'], self._media['region']['height'])
+
+        path = "%s/%s-%s-%s%s" % (
+            self._media['save_dir'],
+            self._media['layout_id'], self._media['region']['id'], self._media['id'],
+            self._media['res_ext']
+        )
+        if 'webpage' == str(self._media['type']) and 'native' == str(self._media['render']):
+            url = self._media['options']['uri']
+            self._widget.load(QUrl.fromPercentEncoding(url))
+        else:
+            self._widget.load('file://' + path)
+        self._widget.show()
+
+        duration = float(self._media['duration']) * 1000
+        self._play_timer.start(duration)
+        self.started_signal.emit()
+
+    @Slot()
+    def stop(self):
+        self.log.debug('stopping_signal.emit(%s)' % self._media['id'])
+        self.stopping_signal.emit(self._media)
+        if self._widget:
+            self._widget.stop()
+            while not self._widget.close():
+                time.sleep(0.25)
+            del self._widget
+            self._widget = None
+
+        self.finished_signal.emit()
+
+
+class MainWindow(QMainWindow):
+    log = logging.getLogger('xiboside.MainWindow')
+
+    def __init__(self, config,  parent=None):
+        super(MainWindow, self).__init__(parent)
+        self._config = config
+
+        self.__layout_id = None
+        self.__layout_time = (0, 0)
+
+        self.__mediaview_list = []
+        self._setup_ui()
+
+        self.__xmds_thread = xthread.XmdsThread(config, self)
+        self.__play_thread = xthread.PlayThread(config, self)
+        self._setup_threads()
+
+        self.__layout_timer = QTimer(self)
+        self.__layout_timer.setSingleShot(True)
+        self.__layout_timer.timeout.connect(self.on_layout_timeout)
+        self.log.setLevel(logging.DEBUG)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop_all_media()
+        self.__xmds_thread.quit()
+        self.__play_thread.quit()
+        if exc_tb or exc_type or exc_val:
+            pass
+
+    def recreate_play_thread(self):
+        self.__play_thread.quit()
+        self.__play_thread = xthread.PlayThread(self._config, self)
+        self.__play_thread.play_layout_sig.connect(self.play_layout)
+        self.__play_thread.play_media_sig.connect(self.play_media)
+        self.__play_thread.start()
+
+    def recreate_xmds_thread(self):
+        self.__xmds_thread.quit()
+        self.__xmds_thread = xthread.XmdsThread(self._config, self)
+        self.__xmds_thread.layout_sig.connect(self.set_layout_id)
+        self.__xmds_thread.downloaded_sig.connect(self.item_downloaded)
+        self.__xmds_thread.start()
+
+    def _setup_threads(self):
+        self.__xmds_thread.layout_sig.connect(self.set_layout_id)
+        self.__xmds_thread.downloaded_sig.connect(self.item_downloaded)
+        self.__play_thread.play_layout_sig.connect(self.play_layout)
+        self.__play_thread.play_media_sig.connect(self.play_media)
+        self.__xmds_thread.start()
+        self.__play_thread.start()
+
+    def _setup_ui(self):
+        self.setCentralWidget(QWidget(self))
+
+    @Slot(str, tuple)
+    def set_layout_id(self, layout_id, layout_time):
+        self.log.debug('set_layout_id(%s, (%d, %d))' %
+                       (layout_id, layout_time[0], layout_time[1]))
+        if self.__layout_id is None:
+            self.__layout_id = layout_id
+            self.__layout_time = layout_time
+            self.__play_thread.set_layout_id(layout_id, layout_time)
+        elif self.__layout_id != layout_id:
+            self.__layout_id = layout_id
+            self.__layout_time = layout_time
+            self.log.debug('set_layout_id got different layout')
+            self.__play_thread.set_layout_id(layout_id, layout_time)
+            self.stop_all_media()
+
+        if self.__layout_time[0] and self.__layout_time[1]:
+            # If it's a scheduled layout, check the stop time, then schedule a stop
+            self.__layout_timer.stop()
+            t1 = layout_time[1] - time.time()
+            self.__layout_timer.start(int(t1 * 1000))
+
+    @Slot()
+    def on_layout_timeout(self):
+        """A timeout slot for expired scheduled-layout"""
+        self.stop_all_media()
+        self.recreate_play_thread()
+        self.recreate_xmds_thread()
+
+    def play_layout(self, layout, layout_id):
+        if self.__layout_id != layout_id:
+            self.__layout_id = layout_id
+
+        if 'bgcolor' in layout:
+            style = "QWidget {background-color: %s;}" % layout['bgcolor']
+            self.setStyleSheet(style)
+
+    def play_media(self, media):
+        if self.__play_thread.playing_media_list.count(media):
+            if 'image' == media['type']:
+                self.play_image_media(media)
+            elif 'video' == media['type']:
+                self.play_video_media(media)
+            else:
+                self.play_web_media(media)
+
+    @Slot()
+    def stop_all_media(self):
+        for view in self.__mediaview_list:
+            with MediaStopping(view) as s:
+                s.wait()
+        del self.__mediaview_list[:]
+
+    @Slot(str, str)
+    def item_downloaded(self, item):
+        if 'layout' == item.type and item.id == self.__layout_id:
+            self.stop_all_media()
+            self.recreate_play_thread()
+
+    def append_mediaview(self, view):
+        view.stopping_signal.connect(self.__play_thread.remove_media)
+        self.__mediaview_list.append(view)
+
+    def play_web_media(self, media):
+        view = WebMediaView(media, self)
+        if not self.__mediaview_list.count(view):
+            view.play()
+            self.append_mediaview(view)
+
+    def play_image_media(self, media):
+        view = ImageMediaView(media, self)
+        if not self.__mediaview_list.count(view):
+            view.play()
+            self.append_mediaview(view)
+
+    def play_video_media(self, media):
+        view = VideoMediaView(media, self)
+        if not self.__mediaview_list.count(view):
+            view.play()
+            self.append_mediaview(view)
---
+++ xthread.py
@@ -0,0 +1,335 @@
+import calendar
+import logging
+import os
+import sys
+import time
+import uuid
+from hashlib import md5
+from threading import Thread, currentThread
+
+from PySide.QtCore import QThread, Signal, Slot
+
+import xlf
+import xmds
+
+
+class XmdsThread(QThread):
+    log = logging.getLogger('xiboside.XmdsThread')
+    downloading_sig = Signal(str, str)
+    downloaded_sig = Signal(object)
+    layout_sig = Signal(str, tuple)
+
+    __xmds_stop = False
+    __xmds_running = False
+
+    def __init__(self, config, parent):
+        super(XmdsThread, self).__init__(parent)
+        self.config = config
+        self.__mac_address = None
+        self.__hardware_key = None
+        self.layout = None
+        self.layout_time = None
+        if not os.path.isdir(config.saveDir):
+            os.mkdir(config.saveDir, 0o700)
+        self.__set_identity()
+        self.xmdsClient = xmds.Client(config.url)
+        self.xmdsClient.set_keys(config.serverKey, self.__hardware_key)
+        self.log.setLevel(logging.DEBUG)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        if exc_tb or exc_type or exc_val:
+            pass
+
+    def __set_identity(self):
+        node = uuid.getnode()  # TODO: Explicit network interface name eg: eth0, wlan0, ...
+        self.__mac_address = ':'.join([str('%012x' % node)[x:x+2] for x in range(0, 12, 2)])
+        url = 'xiboside://%s/%s/%s' % (sys.platform, os.name, self.__mac_address)
+        self.__hardware_key = uuid.uuid3(uuid.NAMESPACE_URL, url)
+
+    @Slot()
+    def stop(self):
+        self.__xmds_stop = True
+        while self.__xmds_running:
+            self.msleep(250)
+            self.log.info('stop() waiting')
+        self.log.info('stop() stopped')
+
+    def __str_to_epoch(self, time_str):
+        seconds = calendar.timegm(time.strptime(time_str, self.config.strTimeFmt))
+        return seconds - self.config.cmsTzOffset
+
+    def __download(self, req_file_entry=None):
+        if not req_file_entry or not req_file_entry.files:
+            return None
+
+        cl = self.xmdsClient
+        self.__is_downloading = True
+        for entry in req_file_entry.files:
+            if self.__xmds_stop:
+                break
+
+            resp = None
+            file_path = None
+            if 'resource' == entry.type:
+                file_path = "{0}/{1}-{2}-{3}{4}".format(self.config.saveDir,
+                                                        entry.layoutid, entry.regionid,
+                                                        entry.mediaid, self.config.res_file_ext)
+
+                param = xmds.GetResourceParam(entry.layoutid, entry.regionid, entry.mediaid)
+                # print 'Downloading {0}'.format(file_path)
+                self.downloading_sig.emit(entry.type, file_path)
+                resp = cl.send_request('GetResource', param)
+            elif entry.type in ('media', 'layout'):
+                file_ext = ''
+                if 'layout' == entry.type:
+                    file_ext = self.config.layout_file_ext
+                file_path = self.config.saveDir + '/' + entry.path + file_ext
+                if md5sum_match(file_path, entry.md5):
+                    # print 'Skipping {0}, md5sum match'.format(file_path)
+                    continue
+
+                param = xmds.GetFileParam(entry.id, entry.type, 0, entry.size)
+                self.downloading_sig.emit(entry.type, file_path)
+                resp = cl.send_request('GetFile', param)
+
+            downloaded = False
+            if resp:
+                f = None
+                try:
+                    f = open(file_path, 'wb')
+                except IOError:
+                    pass
+                finally:
+                    if f:
+                        f.write(resp.content)
+                        f.flush()
+                        os.fsync(f.fileno())
+                        f.close()
+                        downloaded = True
+
+            if downloaded:
+                self.downloaded_sig.emit(entry)
+        # for entry ...
+
+    def __xmds_cycle(self):
+        self.__xmds_running = True
+        self.__xmds_stop = False
+        while not self.__xmds_stop:
+            self.log.info('__xmds_cycle started')
+            cl = self.xmdsClient
+            param = xmds.RegisterDisplayParam(mac_address=self.__mac_address)
+            registered = cl.send_request('RegisterDisplay', param)
+
+            if isinstance(registered, xmds.RegisterDisplayResponse):
+                collect_interval = registered.details.get('collectInterval', 5)
+            else:
+                collect_interval = 5
+
+            self.__download(cl.send_request('RequiredFiles'))
+            schedule = cl.send_request('Schedule')
+            if not schedule:
+                self.msleep(1000)
+                continue
+
+            schedule_found = False
+            if schedule.layouts:
+                for layout in schedule.layouts:
+                    from_time = self.__str_to_epoch(layout.fromdt)
+                    to_time = self.__str_to_epoch(layout.todt)
+                    now_time = time.time()
+                    if from_time <= now_time <= to_time:
+                        self.layout = layout.file
+                        self.layout_time = (from_time, to_time)
+                        schedule_found = True
+                        break  # simultaneous scheduled layout is not supported yet
+                        #  ----+ stop on first scheduled layout
+                # for layout ...
+            # if schedule.layouts ...
+            if not schedule_found:
+                """ play default layout """
+                self.layout = schedule.layout
+                self.layout_time = (0, 0)
+            self.log.debug('emitting layout_sig(%s, (%d, %d))' %
+                           (self.layout, self.layout_time[0], self.layout_time[1]))
+            self.layout_sig.emit(self.layout, self.layout_time)
+            next_collect_time = time.time() + float(collect_interval)
+            while time.time() < next_collect_time and not self.__xmds_stop:
+                self.msleep(250)
+        # while not ...
+        self.__xmds_running = False
+        self.log.info('__xmds_cycle() finished')
+
+    def run(self):
+        if not self.__xmds_running:
+            self.__xmds_cycle()
+        self.log.debug('run() finished %d' % self.exec_())
+
+    def quit(self):
+        self.stop()
+        return super(XmdsThread, self).quit()
+
+
+class PlayThread(QThread):
+    log = logging.getLogger('xiboside.PlayThread')
+    play_layout_sig = Signal(dict, str)
+    play_region_sig = Signal(dict)
+    play_media_sig = Signal(dict)
+    layout_expired_sig = Signal(str)
+
+    playing_media_list = None
+
+    def __init__(self, config, parent):
+        super(PlayThread, self).__init__(parent)
+        self.__play_running = False
+        self.__play_stop = False
+        self.__layout_id = None
+        self.__layout_time = (0, 0)
+        self.config = config
+        if self.playing_media_list is None:
+            self.playing_media_list = []
+        self.log.setLevel(logging.DEBUG)
+
+    @Slot(str)
+    def set_layout_id(self, layout_id, layout_time):
+        self.__layout_id = layout_id
+        self.__layout_time = layout_time
+
+    @Slot()
+    def stop(self):
+        self.__play_stop = True
+        while self.__play_running or len(self.playing_media_list) > 0:
+            self.msleep(200)
+            self.log.info('stop() waiting %d media' % len(self.playing_media_list))
+
+        self.log.info('stop() stopped')
+
+    def __play_cycle(self):
+        self.__play_running = True
+        self.__play_stop = False
+        # last_id = None
+        while not self.__play_stop:
+            if not self.__layout_id:
+                self.msleep(250)
+                continue
+            # if last_id == self.__layout_id:
+            #     same_id = True
+            # else:
+            #     last_id = self.__layout_id
+            #     same_id = False
+
+            file_path = self.config.saveDir + '/' + self.__layout_id + self.config.layout_file_ext
+            xlf_layout = xlf.Xlf(file_path)
+            if xlf_layout:
+                self.play_layout_sig.emit(xlf_layout.layout, self.__layout_id)
+                self.__play_layout(xlf_layout.layout, self.__layout_id)
+                self.log.info('__play_layout finished')
+            # if not same_id:
+            #     pass
+            self.msleep(200)
+        # while not ...
+        self.log.info('__play_cycle finished')
+        self.__play_running = False
+
+    def __play_layout(self, layout, layout_id):
+        if layout is None:
+            return None
+        if self.__play_stop:
+            return None
+
+        threads = []
+
+        for region in layout['regions']:
+            region['layout_id'] = str(layout_id)
+            t = Thread(target=self.__play_region, name='__play_region_{0}'.format(region['id']), args=(region,))
+            t.setDaemon(True)
+            t.start()
+            threads.append(t)
+
+        self.msleep(100)
+        for t in threads:
+            t.join()
+        # while [t.isAlive() for t in threads].count(True) > 0:
+        #     time.sleep(0.25)
+
+    def __play_region(self, region):
+        if not region:
+            return None
+
+        self.play_region_sig.emit(region)
+        self.log.info(currentThread().getName() + ' Starting')
+        for media in region['media']:
+            media['layout_id'] = region['layout_id']
+            media['region'] = {
+                'id': region['id'],
+                'left': region['left'],
+                'top': region['top'],
+                'width': region['width'],
+                'height': region['height']
+            }
+            media['save_dir'] = self.config.saveDir
+            media['res_ext'] = self.config.res_file_ext
+            self.__play_media(media)
+        self.log.info(currentThread().getName() + ' Finished')
+
+    def media_listed(self, media):
+        listed = self.playing_media_list.count(media)
+        return listed
+
+    def remove_media(self, media):
+        self.log.debug('remove_media(%s)' % media['id'])
+        if self.media_listed(media):
+            self.playing_media_list.remove(media)
+            self.log.debug('remove_media(%s) removed' % media['id'])
+
+    def __play_media(self, media):
+        if media is None or self.__play_stop:
+            return None
+
+        self.log.info('__play_media_%s' % media['id'])
+        if self.media_listed(media):
+            self.log.info('Skip playing media %s' % media['id'])
+            return None
+
+        self.play_media_sig.emit(media)
+        self.playing_media_list.append(media)
+
+        while not self.__play_stop:
+            if not self.media_listed(media):
+                break
+            self.msleep(250)
+        self.log.info('__play_media_%s finished' % media['id'])
+
+        self.remove_media(media)
+
+    def run(self):
+        if not self.__play_running:
+            self.__play_cycle()
+        else:
+            self.log.debug('run() skipped, already running...')
+        self.log.debug('run() finished %d' % self.exec_())
+
+    def quit(self):
+        self.stop()
+        return super(PlayThread, self).quit()
+
+
+def md5sum_match(file_path, md5sum):
+    if not os.path.isfile(file_path):
+        return False
+
+    f = None
+    content = None
+    try:
+        f = open(file_path, 'rb')
+    finally:
+        if f:
+            content = f.read()
+        f.close()
+    if not content:
+        return False
+
+    return md5(content).hexdigest() == md5sum

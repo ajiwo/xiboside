3d7c06116715fd78f7df4de5d57716552ec9f950
Amin Riza <aminriza@gmail.com>
2017-02-14 22:09:28 +0700

    cleaner Subscriber exit.

--- xmr.py
+++ xmr.py
@@ -1,3 +1,4 @@
+from Crypto import Random
 import zmq
 
 
@@ -6,24 +7,38 @@ class Subscriber:
         self._url = url
         self._channel = channel
         self._callback = callback
-        self._stop = False
+        self._context = zmq.Context()
+        self._push = None
+        self._stop_command = Random.new().read(16)
 
     def run(self):
-        with zmq.Context() as c:
-            socket = c.socket(zmq.SUB)
-            socket.connect(self._url)
-            socket.setsockopt_string(zmq.SUBSCRIBE, self._channel.decode('ascii'))
-            messages = []
-            while not self._stop:
-                message = socket.recv_string()
-                if message == self._channel:
-                    messages = [message]
-                elif len(messages) > 0:
-                    messages.append(message)
-                if len(messages) == 3:
-                    if callable(self._callback):
-                        self._callback(messages[1:])
+        sub = self._context.socket(zmq.SUB)
+        sub.connect(self._url)
+        sub.setsockopt_string(zmq.SUBSCRIBE, self._channel.decode('ascii'))
 
-    def set_stop(self):
-        self._stop = True
+        push = self._context.socket(zmq.PUSH)
+        port = push.bind_to_random_port('tcp://127.0.0.1')
+        self._push = push
+
+        pull = self._context.socket(zmq.PULL)
+        pull.connect('tcp://127.0.0.1:%d' % port)
+
+        poller = zmq.Poller()
+        poller.register(pull, zmq.POLLIN)
+        poller.register(sub, zmq.POLLIN)
+
+        while True:
+            socks = dict(poller.poll())
+            if sub in socks and socks[sub] == zmq.POLLIN:
+                message = sub.recv_multipart()
+                if len(message) == 3 and self._channel == message[0]:
+                    if callable(self._callback):
+                        self._callback(message[1:])
+            if pull in socks and socks[pull] == zmq.POLLIN:
+                control = pull.recv()
+                if self._stop_command == control:
+                    break
+
+    def stop(self):
+        self._push.send(self._stop_command)
 
